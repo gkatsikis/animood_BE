46e7a32913ad55bbe364b249737860a3111828d1
gkatsikis <geo.bjj.kat@gmail.com>
2022-04-11 14:01:11 -0400

    update animood database functions

--- api/models/animood.py
+++ api/models/animood.py
@@ -10,3 +10,10 @@ class Animood(db.Model):
   story = db.Column(db.String(250))
   created_at = db.Column(db.DateTime, default=datetime.utcnow)
   profile_id = db.Column(db.Integer, db.ForeignKey('proifles.id'))
+
+  def __repr__(self):
+    return f"Animood('{self.id}', '{self.name}'"
+
+  def serialize(self):
+    animood = {a.name: getattr(self, a.name) for a in self.__table__.columns}
+    return animood
